5e1b1a99cfde8deec951920953d8ef997d1e5cc2
Waweru-Steve <wawerusteve142@yahoo.com>
2020-07-02 19:22:42 +0300

    Get single bootcamp, add new bootcamp

--- seeder.js
+++ seeder.js
@@ -1,12 +1,16 @@
 const fs = require('fs')
 const mongoose = require('mongoose')
 const colors = require('colors')
-const dotenv = require('dotenv').config({ path: './config/config.env' })
+const dotenv = require('dotenv')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
 
 // Load models
 const Bootcamp = require('./models/Bootcamp')
 const Course = require('./models/Course')
 const User = require('./models/User')
+const Review = require('./models/Review')
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -23,16 +27,19 @@ const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, 'u
 
 const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8'))
 
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8'))
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps)
     await Course.create(courses)
     await User.create(users)
+    await Review.create(reviews)
     console.log('Data Imported...'.green.inverse)
     process.exit()
-  } catch (error) {
-    console.error(error)
+  } catch (err) {
+    console.error(err)
   }
 }
 
@@ -42,10 +49,11 @@ const deleteData = async () => {
     await Bootcamp.deleteMany()
     await Course.deleteMany()
     await User.deleteMany()
+    await Review.deleteMany()
     console.log('Data Destroyed...'.red.inverse)
     process.exit()
-  } catch (error) {
-    console.error(error)
+  } catch (err) {
+    console.error(err)
   }
 }
 
