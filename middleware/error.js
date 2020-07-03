50c0765ebbaef3d2f7ac442f4820239e6a0ebb61
Waweru-Steve <wawerusteve142@yahoo.com>
2020-07-03 15:32:26 +0300

    Changed castError message

--- middleware/error.js
+++ middleware/error.js
@@ -5,17 +5,18 @@ const errorHandler = (err, req, res, next) => {
 
   error.message = err.message
 
-  // Log to console for the dev
-  console.log(err.stack.red)
+  // Log to console for dev
+  console.log(err)
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
-    const message = `Bootcamp not found with id of ${err.value}`
+    const message = `Resource not found`
     error = new ErrorResponse(message, 404)
   }
+
   // Mongoose duplicate key
   if (err.code === 11000) {
-    const message = `Duplicate field value entered`
+    const message = 'Duplicate field value entered'
     error = new ErrorResponse(message, 400)
   }
 
@@ -25,7 +26,10 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 400)
   }
 
-  res.status(error.statusCode || 500).json({ success: false, error: error.message || 'Server Error' })
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error'
+  })
 }
 
 module.exports = errorHandler
