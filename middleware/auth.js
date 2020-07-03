e4029d8fb2338f7d23c6b8a4fd7c43052b61dc01
Waweru-Steve <wawerusteve142@yahoo.com>
2020-07-03 15:33:17 +0300

    Token cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,29 +8,28 @@ exports.protect = asyncHandler(async (req, res, next) => {
   let token
 
   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1]
+    // Set token from cookie
   }
-
-  // else if(req.cookies.token) {
-  //   token = req.cookies.token
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
   // }
 
   // Make sure token exists
   if (!token) {
-    return new ErrorResponse('Not authorized to access this route', 401)
+    return next(new ErrorResponse('Not authorized to access this route', 401))
   }
 
   try {
     // Verify token
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-    console.log(decoded)
-
     req.user = await User.findById(decoded.id)
 
     next()
-  } catch (error) {
-    return new ErrorResponse('Not authorized to access this route', 401)
+  } catch (err) {
+    return next(new ErrorResponse('Not authorized to access this route', 401))
   }
 })
 
@@ -38,7 +37,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403)
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403))
     }
     next()
   }
